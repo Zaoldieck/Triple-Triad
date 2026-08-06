459c0ad80adac89037532a1406b8e8406d7cf0a6
Zaoldieck <simonevagni@hotmail.com>
2026-08-06 17:53:24 +0200

    Refactor: move pygame initialization and game loop into a Game Class

---
+++ game/game.py
@@ -0,0 +1,45 @@
+import pygame
+
+from screens.main_menu import MainMenu # serve per importare la classe MainMenu dal file main_menu.py
+
+class Game:
+    def __init__(self):
+
+        #inizializzo py game
+        pygame.init()
+
+        # disabilito la ripetizione dei tasti in modo che non scorre velocissimo il menu
+        pygame.key.set_repeat(0)
+
+        # dimensioni della finestra
+        self.width = 1280
+        self.height = 720
+
+        # creo la finestra di gioco
+        self.screen = pygame.display.set_mode((self.width, self.height))
+
+        # creo il menu principale
+        self.main_menu = MainMenu(self.width, self.height)  # creo un'istanza della classe MainMenu
+
+    def run(self): # loop per tenere aperta la finestra di gioco
+        running = True 
+        while running:
+            # 1 EVENTI
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+                # inoltro gli eventi al menu principale
+                self.main_menu.handle_events(event)
+
+            # 2 LOGICA
+            self.main_menu.update()
+
+            # 3 DISEGNO
+            self.main_menu.draw(self.screen)
+
+            pygame.display.flip() # disegna tutto quello che sta nel buffer
+
+
+        #chiude pygame
+        pygame.quit()
--- main.py
+++ main.py
@@ -1,38 +1,8 @@
-import pygame #serve per creare la finestra di gioco
-from screens.main_menu import MainMenu # serve per importare la classe MainMenu dal file main_menu.py
+from game.game import Game
 
-#inizializzo py game
-pygame.init()
-# disabilito la ripetizione dei tasti in modo che non scorre velocissimo il menu
-pygame.key.set_repeat(0)  
 
-# dimensioni della finestra
-width = 1280
-height = 720
+# creo il gioco
+game = Game()
 
-# creo la finestra di gioco
-screen = pygame.display.set_mode((width, height))
-
-#creo il menu principale
-main_menu = MainMenu(width, height)  # creo un'istanza della classe MainMenu spostato a parte
-
-running = True
-while running: #loop per tenere aperta la finestra di gioco
-    # 1 EVENTI
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-        # inoltro gli eventi al menu principale
-        main_menu.handle_events(event)
-
-    # 2 LOGICA
-    main_menu.update()
-
-    # 3 DISEGNO
-    main_menu.draw(screen)
-
-    pygame.display.flip() # disegna tutto quello che sta nel buffer
-
-# Chiude pygame
-pygame.quit()
+# avvio il gioco
+game.run()
